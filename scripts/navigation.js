973b1d307ac5355ae352585ee747217c8a7d6922
VictorReno <v.renzo@usp.br>
2022-01-08 12:14:36 -0300

    add navigation

---
+++ scripts/navigation.js
@@ -0,0 +1,33 @@
+let sectionProjects = document.querySelector(".containerContentProjects");
+let sectionCV = document.querySelector(".containerContentCV");
+let sectionAbout = document.querySelector(".containerContentAbout");
+let sectionHome = document.querySelector(".containerContentHome");
+
+function changeToProjects(){
+  sectionProjects.classList.remove("disabled");
+  sectionCV.classList.add("disabled");
+  sectionAbout.classList.add("disabled");
+  sectionHome.classList.add("disabled");
+  console.log("oi");
+}
+
+function changeToAbout(){
+  sectionProjects.classList.add("disabled");
+  sectionCV.classList.add("disabled");
+  sectionAbout.classList.remove("disabled");
+  sectionHome.classList.add("disabled");
+}
+
+function changeToCV(){
+  sectionProjects.classList.add("disabled");
+  sectionCV.classList.remove("disabled");
+  sectionAbout.classList.add("disabled");
+  sectionHome.classList.add("disabled");
+}
+
+function changeToHome(){
+  sectionProjects.classList.add("disabled");
+  sectionCV.classList.add("disabled");
+  sectionAbout.classList.add("disabled");
+  sectionHome.classList.remove("disabled");
+}
