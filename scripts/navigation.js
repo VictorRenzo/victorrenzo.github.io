5cf6fac5e534926fa1c91c5fb5ed23f2f935af21
VictorReno <v.renzo@usp.br>
2022-01-11 11:41:52 -0300

    add about

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -3,11 +3,24 @@ let sectionCV = document.querySelector(".containerContentCV");
 let sectionAbout = document.querySelector(".containerContentAbout");
 let sectionHome = document.querySelector(".containerContentHome");
 
+let linkNavProjects = document.querySelector(".linkItem.linkProjects");
+let linkNavCV = document.querySelector(".linkItem.linkCV");
+let linkNavAbout = document.querySelector(".linkItem.linkAbout");
+
+let temporary = document.querySelector(".temporary");
+
 function changeToProjects(){
   sectionProjects.classList.remove("disabled");
   sectionCV.classList.add("disabled");
   sectionAbout.classList.add("disabled");
   sectionHome.classList.add("disabled");
+
+  linkNavProjects.classList.remove("linkOff");
+  linkNavProjects.classList.add("linkOn")
+  linkNavAbout.classList.remove("linkOn");
+  linkNavCV.classList.remove("linkOn");
+
+  temporary.classList.remove("disabled");
 }
 
 function changeToAbout(){
@@ -15,6 +28,14 @@ function changeToAbout(){
   sectionCV.classList.add("disabled");
   sectionAbout.classList.remove("disabled");
   sectionHome.classList.add("disabled");
+
+  linkNavAbout.classList.remove("linkOff");
+  linkNavAbout.classList.add("linkOn")
+  linkNavProjects.classList.remove("linkOn");
+  linkNavCV.classList.remove("linkOn");
+
+  temporary.classList.add("disabled");
+
 }
 
 function changeToCV(){
@@ -22,6 +43,13 @@ function changeToCV(){
   sectionCV.classList.remove("disabled");
   sectionAbout.classList.add("disabled");
   sectionHome.classList.add("disabled");
+
+  linkNavCV.classList.remove("linkOff");
+  linkNavCV.classList.add("linkOn")
+  linkNavAbout.classList.remove("linkOn");
+  linkNavProjects.classList.remove("linkOn");
+
+  temporary.classList.remove("disabled");
 }
 
 function changeToHome(){
@@ -29,4 +57,10 @@ function changeToHome(){
   sectionCV.classList.add("disabled");
   sectionAbout.classList.add("disabled");
   sectionHome.classList.remove("disabled");
+
+  linkNavProjects.classList.remove("linkOn");
+  linkNavAbout.classList.remove("linkOn");
+  linkNavCV.classList.remove("linkOn");
+
+  temporary.classList.remove("disabled");
 }
